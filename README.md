4c7aad683b11ff4740269571e48d6a2106f77d11
caowenbin <caowenbin@xuetangx.com>
2018-05-11 11:17:00 +0800

    修改pip setup.py代码

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-## 项目结构
-```
-   - rest_framework/
-     - bin/
-
-```
-安装依赖包
-pip install -r requirements.txt
-
-tornado-fire 查看支持的命令集
-
-表单方面
-1、获得表单字段（比如user_name）默认值，如果值为None, 并form类中存在类似get_<field_name>的函数，即get_user_name()函数，则执行生成对应的字段默认值
-2、表单字段检查
-如果存在类似validate_<field_name>的函数，则执行检查，参数为字段的输入值
+Tornado Rest Framework 框架，可以方便搭建Rest API应用
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from os.path import dirname, join, abspath
 from setuptools import setup, find_packages
-__author__ = 'caowenbin'
 
 HERE = abspath(dirname(__file__))
 
@@ -13,17 +12,21 @@ with open(join(HERE, "requirements.txt"), encoding='utf-8') as f:
 
 
 setup(
-    name='rest_framework',
+    name='tornado-rest-framework',
     version=version,
-    url='',
     description='Tornado Rest Framework',
-    author='binhua',
-    author_email='binhua18@126.com',
+    author='caowenbin',
+    author_email='cwb201314@qq.com',
+    keywords="tornado asyncio api",
+    url='https://github.com/caowenbin/tornado-rest-framework',
+    download_url='https://github.com/caowenbin/tornado-rest-framework',
     license='BSD',
     packages=find_packages(exclude=('admin', 'admin.*', 'rest_framework.test', 'rest_framework.test.*')),
     include_package_data=True,
     zip_safe=False,
-    classifiers=[],
+    classifiers=[
+       'Programming Language :: Python :: 3.6.1',
+    ],
     scripts=['rest_framework/bin/tornado-admin.py'],
     entry_points={'console_scripts': [
         'tornado-admin = rest_framework.core.script:execute_from_command_line',
