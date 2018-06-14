1e88977abf562e34bc134d142a4a85adecbef4fc
caowenbin <caowenbin@xuetangx.com>
2018-06-14 10:22:13 +0800

    调整版本号

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tornado-rest-framework',
-    version="0.1.4",
+    version="0.1.5",
     keywords=("tornado", "asyncio", "rest api", "python3"),
     description='Tornado Rest Framework',
     long_description="Tornado Rest Framework",
